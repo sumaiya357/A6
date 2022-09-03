d5a3a8df7151404035a13023898d5ded9cf0f145
sumaiya357 <rahmansumaiya75@gmail.com>
2022-09-03 11:52:15 +0600

    category added by fetching

--- js/myjs.js
+++ js/myjs.js
@@ -1,1 +1,22 @@
-console.log('connected')
+ const loadNews = ()=>{
+    fetch(' https://openapi.programming-hero.com/api/news/categories')
+    .then(res=>res.json())
+    .then(data=>displayCategory(data.data.news_category
+        ))
+ }
+
+//const newsList = document.getElementById('news-list')
+const displayCategory = categories=>{
+     //console.log(categories)
+    const newsList = document.getElementById('news-list')
+    categories.forEach(category=>{
+        console.log(category)
+        const a= document.createElement('a')
+        a.classList.add('ms-4','text-decoration-none')
+        a.innerHTML=` ${category.category_name}
+        `
+        newsList.appendChild(a)
+    })
+}
+
+loadNews()
